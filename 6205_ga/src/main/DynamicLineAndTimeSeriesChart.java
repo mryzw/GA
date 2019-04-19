624273802ffa8473526a60a3420051efb141cf34
Chen.JL <chen.jialin@husky.neu.edu>
2019-04-19 12:35:24 -0400

    Update DynamicLineAndTimeSeriesChart.java

--- 6205_ga/src/main/DynamicLineAndTimeSeriesChart.java
+++ 6205_ga/src/main/DynamicLineAndTimeSeriesChart.java
@@ -129,6 +129,8 @@ public class DynamicLineAndTimeSeriesChart extends ApplicationFrame implements A
 //        final Millisecond now = new Millisecond();
         this.series.add(new Millisecond(), this.lastValue);
 
+        if (id == 300) timer.stop();
+
 //        System.out.println("Current Time in Milliseconds = " + now.toString()+", Current Value : "+this.lastValue);
     }
 
